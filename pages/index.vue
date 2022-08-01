6d52dd2c18eec71e7a6229eb4474a82277a18721
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-08-01 18:43:55 +0530

    Add second commit of admin form

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-   
+    <NuxtLink v-slot="{ navigate }" to="/admin" custom>
+      <button @click="navigate" class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+         Admin Page
+      </button>
+    </NuxtLink><br><br>
 
    <NuxtLink v-slot="{ navigate }" to="/form" custom>
       <button @click="navigate" class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
