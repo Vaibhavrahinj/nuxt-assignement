8a04e6e961b5a5fc3add550676e915c343926055
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-08-02 17:14:54 +0530

    add pages

--- pages/form.vue
+++ pages/form.vue
@@ -1,19 +1,23 @@
 <template>
+
   <div>
-    <div>
-      <div
-        class="flex flex-wrap gap-1 justify-between w-full lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1"
-      >
-        <div>
+   <div>
           <button
             @click="showSlots"
-            class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out rounded-full"
+            class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out rounded-full"
           >
             showSlots
           </button>
         </div>
+   
+    
+    <div>
+      <div
+        class="flex flex-wrap gap-1 justify-between lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1"
+      >
+        
         <div id="cartBg"
-          class="border-4 border-black w-1/4"
+          class="border-2 border-black w-1/4"
           v-for="item in timeSlots">
           <div>
             <br />
@@ -32,7 +36,7 @@
           <br />
 
           <div>
-            <NuxtLink v-slot="{ book }" to="/admin" custom>
+            
               <button
                 @click="book"
                 type="submit"
@@ -40,39 +44,43 @@
               >
                 Book Now
               </button>
-            </NuxtLink>
           </div>
         </div>
       </div>
     </div>
   </div>
+ <Admin></Admin>
 </template>
 <script>
+//  import Admin1 from '~/pages/admin.vue';
 
+import Admin from '~/pages/admin.vue';
 export default {
-  data() {
-    return {
-      timeSlots: [],
-    };
-  },
- 
-  methods: {
-    showSlots() {
-     var d = new Date("2022-08-01 09:00:00");
-      var e = new Date("2022-08-01 09:00:00");
-      for (let i = 0; i < 20; i++) {
-        d.setMinutes(d.getMinutes() + 10);
-        e.setMinutes(d.getMinutes() - 10);
-        // console.log(d);
-        this.timeSlots.push({
-          startTime: e.toLocaleTimeString(),
-          endTime: d.toLocaleTimeString(),
-        });
-      }
-     
+    data() {
+        return {
+            timeSlots: [],
+        };
     },
-    
-  },
+    methods: {
+        showSlots() {
+            var d = new Date("2022-08-01 09:00:00");
+            var e = new Date("2022-08-01 09:00:00");
+            for (let i = 0; i < 20; i++) {
+                d.setMinutes(d.getMinutes() + 10);
+                e.setMinutes(d.getMinutes() - 10);
+                // console.log(d);
+                this.timeSlots.push({
+                    startTime: e.toLocaleTimeString(),
+                    endTime: d.toLocaleTimeString(),
+                });
+            }
+        },
+        book() {
+            window.confirm("This Slots is Booked Now");
+            document.getElementById("cartBg").style = "background-color:red;";
+        }
+    },
+    components: { Admin }
 };
 
 
